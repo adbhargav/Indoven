cd21e5652ea9951be074db4887cc11b6bd7681aa
adbhargav <bhargavandhe202@gmail.com>
2026-01-15 15:36:28 +0530

    updated whatsupicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ export default {
         sans: ["Inter", "sans-serif"],
       },
     },
+    keyframes: {
+      bounceSlow: {
+        '0%, 100%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
+        '50%': { transform: 'translateY(0)', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)' },
+      }
+    },
+    animation: {
+      'bounce-slow': 'bounceSlow 2s infinite',
+    },
   },
   plugins: [],
 }
